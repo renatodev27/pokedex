cd83a0d5ae1116ce98a3a32dfd9a7f4d8ef81c36
Renato Ramos <renatoramos.devops@gmail.com>
2021-09-26 21:58:16 -0500

    Added final design and functionality

--- js/app.js
+++ js/app.js
@@ -24,15 +24,20 @@ const app = new Vue({
         statSpdef: 0,
         pokeSpeed: '',
         statSpeed: 0,
-        pokeType: '',
+        pokemonTypes: [],
+        prev: null,
+        next: null,
     },
     methods: {
-        listarPokemones() {
-            const url = 'https://pokeapi.co/api/v2/pokemon';
+        listarPokemones(url) {
+            if (!this.next) url = 'https://pokeapi.co/api/v2/pokemon';
             if (!this.listLoading) this.listLoading = true;
+            if (this.pokemonList.length) this.pokemonList = [];
 
             fetch(url).then ( (response) => response.json())
             .then( (pokemons) => {
+                this.prev = pokemons.previous;
+                this.next = pokemons.next;
                 
                 pokemons.results.map( (pokemon) => {
                     const data_url = 'https://pokeapi.co/api/v2/pokemon/'+pokemon.name;
@@ -65,19 +70,30 @@ const app = new Vue({
             synth.speak(utterance);
         },
         obtenerPokemon(pokemonName) {
-            const url = 'https://pokeapi.co/api/v2/pokemon/'+pokemonName;
+            const url = 'https://pokeapi.co/api/v2/pokemon/'+pokemonName.toLowerCase();
             this.isLoading = true;
-            this.searched = true;
             if (synth.speaking) synth.cancel();
 
             if (this.searchPokemon !== '') {
                 fetch(url).then( (response) => response.json(), this.screenOf = false)
                 .then( (data) => {
                     this.pokemon = data;
+
+                    this.pokemon.types.map( type => { 
+                        type['type'] = {
+                            name: type['type'].name,
+                            url: type['type'].url,
+                            color: this.defineColorsByType(type['type'].name)
+                        }
+                    })
+
+                    console.log(this.pokemon.types);
+
                     this.getPokemonStats(this.pokemon.stats, this.pokemon.name);
                     if (this.searchPokemon != '') this.searchPokemon = '';
+                    this.searched = true;
                     this.isLoading = false;
-                    console.log(data);
+                    
                 })
                 .catch( (error) => {
                     if (error) {
@@ -95,11 +111,8 @@ const app = new Vue({
             }
         },
         choosePokemon(data = []) {
-            this.searched = true;
-            this.getPokemonStats(data['stats'], data['name']);
-            this.screenOf = false;
-            this.pokemon = data;
-            console.log(data);
+            this.searchPokemon = data['name'];
+            this.obtenerPokemon(data['name']);
         },
         getPokemonStats(listStats, pokeName) {
             let hp, attack, defense, sp_attack, sp_defense, speed;
@@ -147,9 +160,7 @@ const app = new Vue({
 
             fetch(url).then ( (response) => response.json())
             .then ( (data) => {
-                data.results.map( (type) => {
-                    type
-                })
+                this.pokemonTypes = data.results;
             })
         },
         returnToPokemonList() {
@@ -157,9 +168,44 @@ const app = new Vue({
             this.screenOf = true;
             this.searched = false;
             this.pokemon = [];
+        },
+        nextPrevPokemonList(action) {
+            switch (action) {
+                case 'next' : this.listarPokemones(this.next); break;
+                case 'prev' : this.listarPokemones(this.prev); break;
+            }
+        },
+        defineColorsByType(name) {
+            let color;
+
+            switch (name) {
+                case 'normal' : color = '#7B7B7B'; break;
+                case 'fighting' : color = '#585AF7'; break;
+                case 'flying' : color = '#12C7A9'; break;
+                case 'poison' : color = '#C90C55'; break;
+                case 'ground' : color = '#97682F'; break;
+                case 'rock' : color = '#89755D'; break;
+                case 'bug' : color = '#A24C1B'; break;
+                case 'ghost' : color = '#2E102A'; break;
+                case 'steel' : color = '#686A6D'; break;
+                case 'fire' : color = '#FF3600'; break;
+                case 'water' : color = '#19B2F5'; break;
+                case 'grass' : color = '#13FD00'; break;
+                case 'electric' : color = '#F5B919'; break;
+                case 'psychic' : color = '#19F5A8'; break;
+                case 'ice' : color = '#62C7CD'; break;
+                case 'dragon' : color = '#FF2121'; break;
+                case 'dark' : color = '#383838'; break;
+                case 'fairy' : color = '#F94FA6'; break;
+                case 'unknown' : color = '#AC865B'; break;
+                case 'shadow' : color = '#9EB2AF'; break;
+            }
+
+            return color;
         }
     },
     created() {
+        this.getPokemonsTypes();
         this.listarPokemones();
     },
     mounted() {
